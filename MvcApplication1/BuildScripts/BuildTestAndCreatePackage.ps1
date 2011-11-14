4c2774fb1ed097a40797297728a080594117c84d
Holger Friedrich <holger.friedrich@bekk.no>
2011-11-14 21:20:14 +0100

    Added error handling when calling other scripts.

--- MvcApplication1/BuildScripts/BuildTestAndCreatePackage.ps1
+++ MvcApplication1/BuildScripts/BuildTestAndCreatePackage.ps1
@@ -1,4 +1,14 @@
-﻿cls
+﻿Function CheckForErrors() {
+  if (!$?) {
+	Write-Host "FAILED! STOPPING SCRIPT EXECUTION" -foregroundcolor red
+    exit -1
+  }
+}
+
+cls
 .\BuildApplication.ps1
+CheckForErrors
 .\RunUnitTests.ps1
+CheckForErrors
 .\CreateDeployPackage.ps1
+CheckForErrors
--- MvcApplication1/BuildScripts/installRunWebTestsCreateNuGetAndPushToGallery.ps1
+++ MvcApplication1/BuildScripts/installRunWebTestsCreateNuGetAndPushToGallery.ps1
@@ -37,4 +37,6 @@ $deployPackagePath = Get-Item -Path '.\Content\Pipe.Web.zip'
 DeployWebApplicationSite 'PipeWebTest' 'd:\Temp\PipeWebTest' '*' '80' 'pipeWebTest' 'pipeWebTest' $null $deployPackagePath "$env:ProgramFiles\IIS\Microsoft Web Deploy V2\msdeploy.exe"
 
 .$scriptFileForRunningWebTests
+CheckForErrors
 .$scriptFileForCreatingAndPushingNuGetPackage $versionNumber
+CheckForErrors
